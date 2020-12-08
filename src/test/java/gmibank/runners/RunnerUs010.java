130b699257d1940dae50352ebbc711444c7e7757
Kadir <kadirgok1@gmail.com>
2020-12-08 11:33:17 -0500

    commit2

--- src/test/java/gmibank/runners/RunnerUs010.java
+++ src/test/java/gmibank/runners/RunnerUs010.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
         plugin = {"html:target/default-cucumber-reports",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml"},
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/us010_tc.feature",
         glue = "gmibank/stepdefinitions/",
         tags = "@us001tc010",
          dryRun = true
--- src/test/resources/features/api.feature
+++ src/test/resources/features/api.feature
@@ -1,4 +1,4 @@
-@api
+#@api
 Feature: API Test
   Scenario:TC01_kullanici apinin calistini kontrol eder
     Given kullanici kendisine verilen token ile apiye baglanir
